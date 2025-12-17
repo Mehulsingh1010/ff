784ef7def8bdafd8f5bf3a8f838fd0068202e73d
mehulsingh1010 <mehulsingh076@gmail.com>
2025-12-18 00:40:34 +0530

    community section responsiveness

--- app/_components/Community.tsx
+++ app/_components/Community.tsx
@@ -1,26 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+// Example: import images at top if needed
+// import img1 from "@/public/img1.png";
+// import img2 from "@/public/img2.png";
+// import img3 from "@/public/img3.png";
+// import img4 from "@/public/img4.png";
+// import img5 from "@/public/img5.png";
+// import img6 from "@/public/img6.png";
 "use client";
 export default function Community() {
   return (
     <> 
-    <div className=" text-black rounded-tl-[60px] rounded-tr-[60px] mt-[-60px] relative bg-[#F3ECD2] "> <div className="pt-[170px]"></div>
-      <div className="relative lg:h-[472px] px-4 md:px-[30px] lg:px-[67px] flex flex-col md:flex-col lg:flex-row lg:border-t-3 lg:border-b-3 border-[#D2CCB6] items-center md:items-center lg:items-stretch justify-center md:justify-center lg:justify-start">
-            <div className="hidden lg:block absolute h-[22px] w-screen ml-[-67px] mt-[215px] top-2 border-t-3 border-b-3 text-[transparent] border-[#D2CCB6]">a</div>
+    <div className=" border-t-[1.6px]  lg:border-none text-black rounded-tl-[60px]  rounded-tr-[60px] mt-[-60px] relative bg-[#F3ECD2] "> <div className=" pt-[78px] md:pt-[117px] 2xl:pt-[170px]"></div>
+      <div className=" relative 2xl:h-[472px] xl:h-[445.425px] lg:h-[319.73px] 2xl:gap-[68.2667px] 2xl:pl-[68px]  lg:flex lg:pl-[45px] xl:pl-[64px]  lg:border-t-3 lg:border-b-3 border-[#D2CCB6] ">
+            <div className="block absolute h-[16px] mt-[704px] ml-0 md:mt-[620px] md:h-[20px] md:ml-[0px] lg:h-[14px] lg:ml-[-44px] xl:h-[22px] w-screen  xl:ml-[-70px] lg:mt-[142.6px] xl:mt-[200px] 2xl:mt-[215px] top-2 border-t-3 border-b-3 text-[transparent] border-[#D2CCB6]">a</div>
+                   <div className="md:hidden absolute h-[16px] mt-[540px] ml-0 md:mt-[620px] md:h-[20px] md:ml-[0px] lg:h-[14px] lg:ml-[-44px] xl:h-[22px] w-screen  xl:ml-[-70px] lg:mt-[142.6px] xl:mt-[200px] 2xl:mt-[215px] top-2 border-t-3 border-b-3 text-[transparent] border-[#D2CCB6]">a</div>
 
-       
-        <div className="w-full lg:w-[662px] order-1 md:order-1 lg:order-1 flex flex-col items-center md:items-center lg:items-start">
-          <div className="font-heading-bold pt-[30px] lg:pt-[50px] text-center md:text-center lg:text-left">
-            <p className="text-[32px] lg:text-[68.9722px] leading-[38px] lg:leading-[62.075px]">
+        <div className=" mx-auto my-auto  md:mx-0   px-[13px] md:px-0">
+          <div className="font-heading-bold md:px-[44px] md:max-w-[538.705px] md:mx-auto lg:w-auto xl:w-[521.775px] lg:px-0  ">
+            <p className="leading-[36.9231px]  md:leading-[53.8705px] lg:leading-[41.6px] xl:leading-[58.5px] 2xl:leading-[62.075px] text-[41.0256px] text-center lg:text-[46.2222px] xl:text-[65px] lg:text-left md:text-[59.8561px]   2xl:text-[68.9722px]">
               An Event Ran by <br /> The Community,
             </p>
-            <div className="rotate-[1deg] h-auto lg:h-[69px] w-full lg:w-[555px] bg-[#F3A20F] border-[1.6px] border-black shadow-[rgba(0,0,0,0.2)] rounded-md shadow-[0px_6px_0px_0px_#000] text-nowrap text-white mt-[15px] lg:mt-0">
-              <p className="text-[24px] lg:text-[62.075px] leading-[32px] lg:leading-[55.8675px] px-[15.5188px] py-[8px] lg:pt-[9.31125px] lg:py-0">
-                For The Community
-              </p>
+            <div className=" mx-auto md:mx-0 rotate-[1deg] h-[85.225px] md:h-[62.45px] md:w-[480.7px] lg:h-[47.35px] xl:h-[67.55px] 2xl:h-[73px] w-[229.738px] lg:w-[370.388px] xl:w-[521.775px] 2xl:w-[555px] bg-[#F3A20F] border-[1.6px] border-black shadow-[rgba(0,0,0,0.2)] rounded-md shadow-[0px_6px_0px_0px_#000] text-nowrap text-white">
+           <p className="
+  leading-[32.8205px] md:leading-[48.4835px] 2xl:leading-[55.8675px]
+  text-[41.0256px] md:text-[53.8705px] lg:text-[41.6px] xl:text-[58.5px] xl:pt-[10px] 2xl:text-[62.075px] 
+  pt-[9.31125px] lg:pt-0 
+  text-center
+">
+  For The <span className="md:hidden"><br/></span> Community
+</p>
+
             </div>
           </div>
-          <div className="mt-[20px] lg:mt-[33px]"></div>
+          <div className="mt-[26px] 2xl:mt-[33px]"></div>
 
-          <div className="h-auto lg:h-[66px] w-full lg:w-[475px] text-[14px] lg:text-[16.9778px] tracking-[0.3px] lg:leading-[22.0711px] leading-[18px] font-regular text-center md:text-center lg:text-left">
+<div className="h-[68.25px] md:h-[57.45px] xl:h-[62.4px] 2xl:h-[66px] w-[293.75px] md:w-[412.538px] lg:w-[318.575px] xl:w-[448px] 2xl:w-[475px] mx-auto text-center lg:text-left text-[13.1282px] md:text-[14.7338px] lg:text-[11.3778px] xl:text-[16px] 2xl:text-[16.9778px] leading-[17.0667px] md:leading-[19.154px] lg:leading-[14.7911px] xl:leading-[20.8px] 2xl:leading-[22.0711px] font-regular">
             <p className="font-quicksand">
               This is a <span className="font-bold">non-profit</span> event run
               by
@@ -32,25 +46,27 @@ export default function Community() {
             </p>
           </div>
 
-          <div className="pt-[20px] lg:pt-[33px]"></div>
-          <div className="pt-[6px]">
+          <div className=" pt-[26px] lg:pt-[12px] xl:mt-[20px] 2xl:pt-[33px]"></div>
+          <div className="pt-[6px] md:pt-0">
             {" "}
             <button
               className="
               cursor-pointer
               
               flex items-center justify-center
+              md:mx-auto lg:mx-0
               border-2 border-[#121212]
               rounded-[170.667px]
-              w-[138.5px] h-[46.925px]
+              w-[129.312px] md:w-[119.988px] lg:w-[91.7875px] xl:w-[130.075px] 2xl:w-[138.5px]  h-[45.125px] md:h-[40.5125px] lg:h-[31.2875px] xl:h-[44px] 2xl:h-[46.925px]
               px-[21.3333px] py-[1.70667px]
-              text-[#121212] text-[17.0667px] font-normal leading-[17.0667px]
+              text-[#121212]  text-[14.7692px] shadow-none md:text-[14.7338px] lg:text-[11.3778px] xl:text-[16px] 2xl:text-[17.0667px] font-normal leading-[17.0667px]
               bg-[#f489a3]
               whitespace-nowrap
-              shadow-[0px_4.26667px_0px_0px_rgba(0,0,0,0.15)]
+              md:shadow-[0px_4.26667px_0px_0px_rgba(0,0,0,0.15)]
               transition-all duration-200 ease-out
               hover:translate-y-[4.26667px] hover:shadow-none
               active:translate-y-[4.26667px] active:shadow-none
+              mx-auto 
             "
             >
               Buy Tickets
@@ -58,18 +74,18 @@ export default function Community() {
           </div>
         </div>
 
-        <div className="pl-0 lg:pl-[67px] flex items-center justify-center w-full lg:w-auto order-2 md:order-2 lg:order-2 mb-[30px] md:mb-[30px] lg:mb-0">
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3 md:gap-5 lg:gap-5 w-full md:w-auto lg:w-auto">
+       <div className="border-t-3 lg:border-none border-b-3 border-[#D2CCB6]  2xl:pl-[67px] mt-[66px] md:pt-0 md:mt-[74px] lg:mt-0 lg:pl-[80px] flex items-center xl:justify-end xl:pr-[53px] justify-center w-full ">
+          <div className="  grid 2xl:grid-cols-3 md:grid-cols-3 lg:gap-[11.3778px] xl:gap-[16px] 2xl:gap-[17.0667px] grid-cols-2  gap-[13.1282px] px-[13px]">
             {[
               {
                 src: "https://cdn.prod.website-files.com/682310547ba9eeb97324a89e/682c8b372a58a085d771d7ef_isabel-adwards.avif",
                 name: "Isabel Edwards",
-                tilt: 3,
+                tilt: 3, // tilt right 3deg
               },
               {
                 src: "https://cdn.prod.website-files.com/682310547ba9eeb97324a89e/682c8b4353290cb0e6475221_josh-fry.avif",
                 name: "Josh Fry",
-                tilt: -3,
+                tilt: -3, // tilt left 3deg
               },
               {
                 src: "https://cdn.prod.website-files.com/682310547ba9eeb97324a89e/682c8b37993d2c764032cfd9_benn-raistrick.avif",
@@ -94,7 +110,7 @@ export default function Community() {
             ].map((item, index) => (
               <div
                 key={index}
-                className="relative w-full lg:w-[206px] h-[140px] md:h-[227px] lg:h-[227px] transition-transform duration-300"
+                className=" relative w-[137.087px] 2xl:w-[207.262px] lg:w-[138.712px] xl:w-[194.113px] md:w-[213.488px] h-[150.788px] md:h-[234.825px] lg:h-[152.575px] xl:h-[213.512px] 2xl:h-[227.988px] transition-transform duration-300"
                 style={{ transform: "rotate(0deg)" }}
                 onMouseEnter={(e) =>
                   (e.currentTarget.style.transform = `rotate(${item.tilt}deg)`)
@@ -108,7 +124,7 @@ export default function Community() {
                   alt={item.name}
                   className="w-full h-full object-cover rounded-3xl border-2"
                 />
-                <div className="absolute h-auto md:h-[36px] lg:h-[36px] rounded-2xl text-center w-[calc(100%-12px)] bottom-2 left-[6px] md:w-[172px] lg:w-[172px] md:ml-[18px] lg:ml-[18px] md:bottom-4 lg:bottom-4 bg-white text-black border-[1.6px] text-[12px] md:text-[16.9778px] lg:text-[16.9778px] font-bold px-2 py-1">
+                <div className="left-1/2 -translate-x-1/2 absolute h-[28.6375px] md:h-[31.7375px] lg:h-[23.6375px] xl:h-[34.2px] 2xl:h-[36px] text-nowrap  rounded-2xl text-center w-[117.387px] md:w-[184.012px] lg:w-[115.95px] xl:w-[162.113px] 2xl:w-[172px] flex justify-center items-center bottom-[9.84616px] md:bottom-[13px] xl:bottom-[15.5152px] bg-white text-black border-[1.6px]  text-[13.1282px] md:text-[14.7338px] lg:text-[11.3778px] xl:text-[15px] xl: 2xl:text-[16.9778px] font-bold px-2 py-1">
                   {item.name}
                 </div>
               </div>
