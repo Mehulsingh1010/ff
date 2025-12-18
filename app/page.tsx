cd85c2106fb4981b435d69eb157cdaa026f89822
mehulsingh1010 <mehulsingh076@gmail.com>
2025-12-18 11:43:54 +0530

    minor fix in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,57 @@
 /* eslint-disable react-hooks/set-state-in-effect */
-'use client'
-import { useState, useEffect } from 'react'
+"use client";
+import { useState, useEffect } from "react";
 
-import WTE from './_components/WTE'
-import Community from './_components/Community'
-import Testimonial from './_components/Testimonial'
-import FAQ from './_components/FAQ'
-import CTO from './_components/CTO'
-import Footer from './_components/Footer'
-import Team from './_components/Team'
-import About from './_components/About' 
-import SunPage from './_components/Sun'
-import ScrollTriggerStrips from './_components/Shower'
-import ScrollTriggerStrips2 from './_components/RotateX'
-import StripesHomeMain from './_components/StripesHome'
+import WTE from "./_components/WTE";
+import Community from "./_components/Community";
+import Testimonial from "./_components/Testimonial";
+import FAQ from "./_components/FAQ";
+import CTO from "./_components/CTO";
+import Footer from "./_components/Footer";
+import Team from "./_components/Team";
+import About from "./_components/About";
+import SunPage from "./_components/Sun";
+import StripesHomeMain from "./_components/StripesHome";
 
 function App() {
-  const [navVisible, setNavVisible] = useState(false)
-  const [preloaderComplete, setPreloaderComplete] = useState(false)
-  const [isPopupOpen, setIsPopupOpen] = useState(false) 
+  const [navVisible, setNavVisible] = useState(false);
+  const [preloaderComplete, setPreloaderComplete] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   useEffect(() => {
-    if (preloaderComplete && !isPopupOpen) { 
+    if (preloaderComplete && !isPopupOpen) {
       const timer = setTimeout(() => {
-        setNavVisible(true)
-      }, 300)
+        setNavVisible(true);
+      }, 300);
 
-      return () => clearTimeout(timer)
-    } 
-    else if (isPopupOpen) {
-      setNavVisible(false)
+      return () => clearTimeout(timer);
+    } else if (isPopupOpen) {
+      setNavVisible(false);
     }
-    
-  }, [preloaderComplete, isPopupOpen]) 
-
-  const isNavbarVisible = navVisible && !isPopupOpen
+  }, [preloaderComplete, isPopupOpen]);
 
   return (
-    <div className='overflow-hidden'>
-   
+    <div className="overflow-hidden">
+      <SunPage svgSrc="/svg/sun.svg" />
+      <StripesHomeMain
+        onPreloaderComplete={() => {
+          setPreloaderComplete(true);
+        }}
+      />
 
-      {/* <SunPage svgSrc="/svg/sun.svg" />
-      <StripesHomeMain onPreloaderComplete={()=>{setPreloaderComplete(true)}} />
-   
       <About onPopupChange={setIsPopupOpen} />
-      
-      <Team /> 
+
+      <Team />
       <WTE />
-      <Community/>  
+      <Community />
 
-      
-      <Testimonial /> */}
-      <FAQ/>
-      <CTO/>
-    
-      <Footer/>
+      <Testimonial />
+      <FAQ />
+      <CTO />
 
-        <>
-        {/* <ScrollTriggerStrips/> */}
-        {/* <ScrollTriggerStrips2/> */}
-        </>
-
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
